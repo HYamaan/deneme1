2a87d085a20b7529161b0c8e23e471d57b36417c
Parabeac-Cloud <admin@parabeac.com>
2023-04-02 23:22:50 +0000

    Auto-generated commit by Parabeac Cloud

--- lib/main.dart
+++ lib/main.dart
@@ -1,229 +1,115 @@
-// Tip: tap on the down arrow to format the code.
-
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'loginPage.dart';
+
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: Center(child: MyWidget()),
-      ),
-    ),
-  );
+  runApp(const MyApp());
 }
-class MyWidget extends StatelessWidget {
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-
-    // Paste it here.
-    // Replace return Text(...); with return YourWidget(...);
-
-    return Container(
-      width: 390,
-      height: 844,
-      color: Color(0xff453a72),
-      padding: const EdgeInsets.only(left: 14, right: 31, top: 35, bottom: 210, ),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children:[
-
-          SizedBox(height: 21.14),
-          Container(
-            width: 192.03,
-            height: 99.18,
-            child: Stack(
-              children:[Container(
-                width: 192.03,
-                height: 99.18,
-              ),
-              ],
-            ),
-          ),
-          SizedBox(height: 21.14),
-          SizedBox(
-            width: 143,
-            height: 48,
-            child: Text(
-              "Sign Up",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: Color(0xff7b34fe),
-                fontSize: 40,
-              ),
-            ),
-          ),
-          SizedBox(height: 21.14),
-          Container(
-            height: 82,
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children:[
-                Text(
-                  "Email",
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 16,
-                  ),
-                ),
-
-
-                
-                SizedBox(height: 6),
-                Container(
-                  width: 343,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(16),
-                    border: Border.all(color: Color(0xff565dfa), width: 1.50, ),
-                  ),
-                  padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 16, ),
-                  child: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children:[
-                      Container(
-                        width: 19.20,
-                        height: 19.18,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(8),
-                          color: Color(0xff565dfa),
-                        ),
-                      ),
-                      SizedBox(width: 15),
-                      Text(
-                        "Ex: abc@example.com",
-                        style: TextStyle(
-                          color: Color(0xffc7c7c7),
-                          fontSize: 16,
-                          fontStyle: FontStyle.italic,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ),
-          SizedBox(height: 21.14),
-          Container(
-            height: 82,
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children:[
-                Text(
-                  "Your Name",
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 16,
-                  ),
-                ),
-                SizedBox(height: 6),
-                Container(
-                  width: 343,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(16),
-                    border: Border.all(color: Color(0xff565dfa), width: 1.50, ),
-                  ),
-                  padding: const EdgeInsets.symmetric(horizontal: 22, vertical: 16, ),
-                  child: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children:[
-                      Container(
-                        width: 16.60,
-                        height: 19.06,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(8),
-                          border: Border.all(color: Color(0xff565dfa), width: 0.60, ),
-                          color: Color(0xff565dfa),
-                        ),
-                      ),
-                      SizedBox(width: 17),
-                      Text(
-                        "Ex. Saul Ramirez",
-                        style: TextStyle(
-                          color: Color(0xffc7c7c7),
-                          fontSize: 16,
-                          fontStyle: FontStyle.italic,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ),
-          SizedBox(height: 21.14),
-          Container(
-            height: 81,
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children:[
-                Text(
-                  "Your Password",
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 16,
-                  ),
-                ),
-              ],
-            ),
-          ),
-
-          Container(
-            padding: const EdgeInsets.only(left: 138, right: 123),
-            child: Row(
-              mainAxisSize: MainAxisSize.min,
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children:[
-                SizedBox(
-                  width: 84,
-                  height: 36,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) =>  LoginPage()),
-                      );
-                    },
-                    style: ElevatedButton.styleFrom(
-                      shadowColor: const Color(0xFFA5A5A5),
-                      backgroundColor: const Color(0xFF5C69E5),
-                      foregroundColor: Colors.white,
-                      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-                      textStyle: GoogleFonts.getFont(
-                        'Roboto',
-                        color: Colors.white,
-                        fontSize: 13,
-                      ),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(4),
-                      ),
-                      elevation: 2,
-                    ),
-                    child: const Text(
-                      'Button',
-                      textAlign: TextAlign.center,
-                    ),
-
-                  ),
-                )
-              ],
-            ),
-          ),
-        ],
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
       ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Invoke "debug painting" (press "p" in the console, choose the
+          // "Toggle Debug Paint" action from the Flutter Inspector in Android
+          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+          // to see the wireframe for each widget.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
